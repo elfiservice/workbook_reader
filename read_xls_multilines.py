e81002030a00ca3023f25c00aed9aae579336830
Ant ARmando Junior <aaraujo@descartes.com>
2022-04-14 11:03:28 -0300

    feat : provided the loop to run in each month justo to C colunm

--- read_xls_multilines.py
+++ read_xls_multilines.py
@@ -1,22 +1,6 @@
 from openpyxl import load_workbook
 
-# CONTROLs
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Agu", "Sep", "Oct", "Nov", "Dec"]
-wb_month_ref = {
-    "Jan": "C2",
-    "Feb": "C11",
-    "Mar": "C19",
-    "Apr": "C27",
-    "May": "C35",
-    "Jun": "C19",
-    "Jul": "C19",
-    "Agu": "C19",
-    "Sep": "C19",
-    "Oct": "C19",
-    "Nov": "C19",
-    "Dec": "C19",
-}
-
 wk_to_take_values = None
 wk_to_receive_values = None
 
@@ -27,8 +11,25 @@ def load_work_books():
     wk_to_take_values = load_workbook(filename = 'planilha-rentabilidade-2022-Editavel.xlsm', data_only=True)
     wk_to_receive_values = load_workbook(filename = "planilha-rentabilidade-2022-Consolidado-Editavel.xlsm")
 
+def run_job():
+    row_init_reference: int = 3
+    rows_in_each_month: int = 4
+    rows_to_next_month: int = 4
+
+    for month in months:
+        for row in range(rows_in_each_month):
+            cell_row = row + 1
+            print("Mes {} linha {}".format(month, cell_row))
+
+            cell = "C{}".format(row_init_reference)
+            print("Cell {}".format(cell))
+            # chamar a função para pegar o valor e jogar na outra planilha
+            row_init_reference = row_init_reference  + 1
+
+        row_init_reference = row_init_reference + rows_to_next_month
+
 def init():
     load_work_books()
-
+    run_job()
 
 init()
